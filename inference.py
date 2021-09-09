f1d72c193ee0df510f3df6fe92f77c320e407150
francesco <taioli.francesco98@gmail.com>
2021-09-09 15:51:10 +0200

    add conf_level on eval

--- inference.py
+++ inference.py
@@ -1,10 +1,11 @@
 import torch
 
 
-def inference(img_path, conf_level=0.4, save_path='predicted'):
+def inference(img_path, conf_level=0.6, save_path='predicted'):
     model = torch.hub.load('ultralytics/yolov5', 'custom',
                            path='model/nfl_weights.pt',
                            )
+    model.conf = conf_level
     model.eval()
     bounding_boxes = model(img_path)
     bounding_boxes.save(save_path)
